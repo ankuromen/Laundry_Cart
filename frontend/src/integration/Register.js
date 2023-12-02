4908788099c3e0bb57c590bf4c6fadceffd4d231
Ankur Patel <ankur.patel07@outlook.com>
2023-12-03 03:57:37 +0530

    Update Register.js

--- frontend/src/integration/Register.js
+++ frontend/src/integration/Register.js
@@ -50,7 +50,7 @@ const Register = () => {
       redirect: 'follow'
     };
 
-    fetch("http://localhost:5055/api/register", requestOptions)
+    fetch("https://laundry-cart-api.vercel.app/api/register", requestOptions)
       .then(response => response.json())
       .then(result => { console.log(result); })
       .catch(error => console.log('error', error));
